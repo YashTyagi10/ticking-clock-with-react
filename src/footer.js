6a451542347e7cce6b8e910c3db87e3df1af063b
Yash Tyagi <tyagiyash721@gmail.com>
2020-12-26 15:48:34 +0530

    First Commit

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h2>View Code: <a href="https://github.com/YashTyagi10/ticking-clock-with-react.git" target="_blank">Click Here</a></h2>
+        </div>
+    );
+};
+
+export default Footer;
